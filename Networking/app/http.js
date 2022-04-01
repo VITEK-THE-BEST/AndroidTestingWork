ac449aed09ebf5af0a6c5921cc347516dbf02bc6
VITEK_THE_BEST <viktor.luka.00@mail.ru>
2022-04-01 23:06:56 +0500

    окончил, ненавиже верстку на native cript

--- Networking/app/http.js
+++ Networking/app/http.js
@@ -8,22 +8,7 @@ export function test() {
         console.log(response)
         console.log("__________________")
         value = response;
-    })
-        .catch(e => {
-            console.log("__e__e___e___________")
-            console.error(e);
-            console.log("__________________")
-        });
-    return value;
-    Http.request({
-        url: 'http://10.0.2.1:22/api/test',
-        method: 'GET',
-        headers: { "Content-Type": "application/json" },
-    }).then(response => {
-        console.log("response__response____________response____")
-        console.log(response)
-        console.log("__________________")
-        value = response;
+        return response;
     })
         .catch(e => {
             console.log("__e__e___e___________")
@@ -32,9 +17,9 @@ export function test() {
         });
     return value;
 }
-export function getUsers() {
+export function reqGetUsers() {
     const value = [];
-    Http.getJSON('http://127.0.0.1/api/users').then(response => {
+    Http.getJSON('http://10.0.2.2:8000/api/users').then(response => {
         value = response;
     })
         .catch(e => {
@@ -42,20 +27,25 @@ export function getUsers() {
         });
     return value;
 }
-export function getDollar() {
-    const value = [];
-    Http.getJSON('http://127.0.0.1:8000/api/dollarPrice').then(response => {
-        value = response;
+export function reqGetDollar() {
+    var value = [];
+
+    return Http.getJSON('http://10.0.2.2:8000/api/dollarPrice').then(response => {
+        console.log("1response__response____________response____");
+        console.log("2response__response____________response____");
+        console.log(response);
     })
         .catch(e => {
+            console.log("1__e__e___e___________")
             console.error(e);
+            console.log("2__e__e___e___________")
         });
-    return value;
+
 }
 
-export function getaeroflot() {
+export function reqGetaeroflot() {
     const value = [];
-    Http.getJSON('http://127.0.0.1:8000/api/aeroflot').then(response => {
+    Http.getJSON('http://10.0.2.2:8000/api/aeroflot').then(response => {
         value = response;
     })
         .catch(e => {
